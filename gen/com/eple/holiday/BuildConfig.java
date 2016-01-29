bf9252868a5bef4544f2456b71b462e90167ee23
winnd <AS40879@yeah.net>
2016-01-29 21:12:49 +0800

    项目提交

---
+++ gen/com/eple/holiday/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.eple.holiday;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/exp/fragment/Page1.java
@@ -0,0 +1,69 @@
+package com.exp.fragment;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.eple.holiday.R;
+
+import com.exp.java.Item_page1;
+
+import android.R.integer;
+import android.app.Fragment;
+import android.app.DownloadManager.Request;
+import android.content.ClipData.Item;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+public class Page1 extends Fragment {
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		// TODO 自动生成的方法存根
+		View view = inflater.inflate(R.layout.page1, container, false);
+		return view;
+	}
+/*
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		// TODO 自动生成的方法存根
+		super.onCreate(savedInstanceState);
+
+		ListView page1_lv = (ListView) this.getActivity().findViewById(
+				R.id.page1_lv);
+		page1_lv.setAdapter(new SimpleAdapter(getActivity(), getData(),
+				R.layout.page1item, new String[] { "title","content","pic" },
+				new int[] { R.id.page1item_title,R.id.page1item_content,R.id.page1item_pic }));
+	}
+
+	private List<Map<String, Object>> getData() {
+		List<Map<String, Object>> lists = new ArrayList<Map<String, Object>>();
+
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("title", "我的第一个条目");
+		map.put("content", "这是描述");
+		map.put("pic", R.drawable.ic_launcher);
+		lists.add(map);
+		
+		map = new HashMap<String, Object>();
+		map.put("title", "我的第一个条目");
+		map.put("content", "这是描述");
+		map.put("pic", R.drawable.ic_launcher);
+		lists.add(map);
+		
+		map = new HashMap<String, Object>();
+		map.put("title", "我的第一个条目");
+		map.put("content", "这是描述");
+		map.put("pic", R.drawable.ic_launcher);
+		lists.add(map);
+
+		return lists;
+	}*/
+}
