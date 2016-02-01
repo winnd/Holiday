5eb2a3e93c2e161f2365e4f46e0d998f250b61c5
winnd <AS40879@yeah.net>
2016-02-01 19:59:48 +0800

    2月1日第二次实验

---
+++ src/com/exp/pageFragment/Page3.java
@@ -0,0 +1,21 @@
+package com.exp.pageFragment;
+
+import com.eple.holiday.R;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Page3 extends Fragment {
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		// TODO 自动生成的方法存根
+		View view = inflater.inflate(R.layout.page3, container,false);
+		return view;
+	}
+
+}
---
+++ src/com/exp/pageFragment/Page4.java
@@ -0,0 +1,19 @@
+package com.exp.pageFragment;
+
+import com.eple.holiday.R;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Page4 extends Fragment{
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View view = inflater.inflate(R.layout.page4, container,false);
+		return view;
+	}
+}
